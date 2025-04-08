340bb29d5ef0cc8dfe76c424d384ef477a5f004c
Hossein <76825182+H0sin@users.noreply.github.com>
2025-04-08 05:56:12 -0700

    fixed sh file

--- generate_certs.sh
+++ generate_certs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir -p certs
+echo "✅ Generate Certs Folder Success"
 
+echo "💫 START GENERATING"
 openssl req -x509 -newkey rsa:4096 -nodes -keyout certs/server.key \
 -out certs/server.crt -days 365 \
 -subj "/CN=grpc-server"
+echo "💫 FINISH GENERATING"
 
 echo "✅ Certificates generated in ./certs"
--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#docker run -v /certs/:/usr/src/marz-g-node/certs -p 50051:50051 marz-g-node
-docker run --network host -v /certs/:/usr/src/marz-g-node/certs marz-g-node
+docker run -v /certs/:/usr/src/marz-g-node/certs -p 50051:50051 marz-g-node
+docker run --network hos  -v /certs/:/usr/src/marz-g-node/certs marz-g-node
