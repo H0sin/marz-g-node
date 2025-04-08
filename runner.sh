0abfc151a74f1aa721b07b265af8ec338b64312c
Hossein <76825182+H0sin@users.noreply.github.com>
2025-04-08 06:19:01 -0700

    docker stoped

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker stop marz-g-node
+docker rm marz-g-node
 docker build -t marz-g-node .
 docker run -v /certs/:/usr/src/marz-g-node/certs -p 50051:50051 --name marz-g-node marz-g-node
 #docker run --network hos  -v /certs/:/usr/src/marz-g-node/certs marz-g-node
