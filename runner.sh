1cf2704264c391baa476c4e8c81e01806f86e913
Hossein <76825182+H0sin@users.noreply.github.com>
2025-04-10 08:47:16 -0700

    config generate certs

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 docker stop marz-g-node
 docker rm marz-g-node
 docker build -t marz-g-node .
-#docker run -e IP_ADDRESS=167.235.20.19 -v /certs/:/usr/src/marz-g-node/certs -p 50051:50051 --name marz-g-node marz-g-node
+
 docker run -d \
   --name marz-g-node \
   --user root \
@@ -15,6 +15,3 @@ docker run -d \
   --cap-add NET_ADMIN \
   --cap-add NET_RAW \
   marz-g-node
-
-
-#docker run --network hos  -v /certs/:/usr/src/marz-g-node/certs marz-g-node
